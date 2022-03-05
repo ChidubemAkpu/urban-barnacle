22bd0a3a8c06b03e36dda36579bd30808ee18430
Chidubem Akpu <stanleychidubemakpu@gmail.com>
2022-03-05 18:17:54 +0100

    Turtle program

---
+++ python2.py
@@ -0,0 +1,26 @@
+#Day 2
+#Creating turtle diagrams
+
+#Turtle projects
+import turtle
+
+screen = turtle.Screen()
+diagram = turtle.Turtle()
+screen.bgcolor("red")
+diagram.color("black")
+
+diagram.pensize(3)
+diagram.forward(50)
+diagram.left(90)
+diagram.forward(50)
+diagram.left(90)
+diagram.forward(50)
+diagram.left(90)
+diagram.forward(50)
+
+screen.mainloop()
+
+
+
+
+
