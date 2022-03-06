c8cef48e54ed1713cc864b10b12c6a4f0067733d
Chidubem Akpu <stanleychidubemakpu@gmail.com>
2022-03-06 23:50:46 +0100

    Drawing polygons using turtle

---
+++ python3.py
@@ -0,0 +1,25 @@
+#For loop
+#Drawing regular polygons of any sides
+
+import turtle
+
+screen = turtle.Screen()
+diagram = turtle.Turtle()
+
+poly_num_side =int(screen.numinput(title="Polygon's side number",
+prompt="What is the polygon's number of sides?",default=None,minval=3,maxval=None))
+
+
+poly_side_length = int(screen.numinput(title="Polygon's sides lenths",
+prompt="What is the polygon's sides length?",default=None,minval=0,maxval=None))
+
+poly_int_angle = int(180 * (poly_num_side - 2) / poly_num_side)
+
+
+for i in range(poly_num_side):
+    diagram.forward(poly_side_length)
+    diagram.left(180 - poly_int_angle)
+
+screen.mainloop()
+    
+
